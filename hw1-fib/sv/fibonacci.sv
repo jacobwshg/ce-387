45a20a14ad4d07901907a2b214df1c270ea9c6b9
jacobwshg <wsg438@gmail.com>
2026-01-08 18:58:08 -0600

    hw1 draft

--- hw1-fib/sv/fibonacci.sv
+++ hw1-fib/sv/fibonacci.sv
@@ -7,30 +7,85 @@ module fibonacci(
   output logic done
 );
 
-  // TODO: Add local logic signals
-  enum logic [1:0] 
+  typedef enum logic [1:0] 
   {
-    IDLE, RUN, DONE,
+    S_IDLE, 
+    S_RUN
   }
-  state;
+  state_t;
 
+  state_t state;
+  state_t nx_state;
+
+  typedef logic[15:0] u16_t;
+  u16_t counter, r0, r1;
+  u16_t nx_counter, nx_r0, nx_r1;
+
+  /* state regs */
   always_ff @ (posedge clk, posedge reset)
   begin
     if ( reset == 1'b1 ) 
     begin
-       // TODO: Implement reset signals
+      /* Implement reset signals */
+      state <= S_IDLE;
+      counter <= 16'd0;
+      r0 <= 16'd0;
+      r1 <= 16'd1;
     end 
     else 
     begin
-       // TODO: Implement clocked signals
+      /* Implement clocked signals */
+      state <= nx_state;
+      counter <= nx_counter;
+      r0 <= nx_r0;
+      r1 <= nx_r1; 
     end
   end
 
-  always_comb 
+  /* next state logic */
+  always_comb
   begin
-    case (state)
-       // TODO: Implement FSM
+    nx_state = S_IDLE;
+    nx_counter = 16'd0;
+    nx_r0 = 16'd0;
+    nx_r1 = 16'd1;
+    unique case ( state )
+      S_IDLE:
+      begin
+        nx_state = start? S_RUN: S_IDLE;
+        nx_counter = counter;
+        nx_r0 = r0;
+        nx_r1 = r1;
+      end
+      S_RUN:
+      begin
+        if ( done )
+        begin
+          nx_state = S_IDLE;
+          nx_counter = counter;
+          nx_r0 = r0;
+          nx_r1 = r1;
+        end
+        else
+        begin
+          nx_state = S_RUN;
+          nx_counter = counter + 1;
+          nx_r0 = r1;
+          nx_r1 = r0 + r1;
+        end
+      end
+      default:
+      begin
+      end
     endcase
   end
+
+  /* output logic */
+  always_comb
+  begin
+    dout = r1;
+    done = (counter >= din);
+  end
+
 endmodule
 
--- hw1-fib/sv/fibonacci_tb.sv
+++ hw1-fib/sv/fibonacci_tb.sv
@@ -15,45 +15,48 @@ module fibonacci_tb;
   // Clock Generator
   always
   begin
-	clk = 1'b0;
-	#5;
-	clk = 1'b1;
-	#5
+    clk = 1'b0;
+    #5;
+    clk = 1'b1;
+    #5;
   end
 
   initial
   begin
-	// Reset
-	#0 reset = 0;
-	#10 reset = 1;
-	#10 reset = 0;
-	
-	/* ------------- Input of 5 ------------- */
-	// Inputs into module/ Assert start
-	#10;
-	din = 16'd5;
-	start = 1'b1;
-	#10 start = 1'b0;
-	
-	// Wait until calculation is done	
-	#10 wait (done == 1'b1);
+    // Reset
+    #0 reset = 0;
+    #10 reset = 1;
+    #10 reset = 0;
 
-	// Display Result
-	$display("-----------------------------------------");
-	$display("Input: %d", din);
-	if (dout === 5)
-	    $display("CORRECT RESULT: %d, GOOD JOB!", dout);
-	else
-	    $display("INCORRECT RESULT: %d, SHOULD BE: 5", dout);
+    /* ------------- Input of 5 ------------- */
+    // Inputs into module/ Assert start
+    #10;
+    din = 16'd5; // change
+    start = 1'b1;
+    #10 start = 1'b0;
+
+    // Wait until calculation is done	
+    #10 wait (done == 1'b1);
+
+    // Display Result
+    $display("-----------------------------------------");
+    $display("Input: %d", din);
+    if (dout === 5)
+      $display("CORRECT RESULT: %d, GOOD JOB!", dout);
+    else
+      $display("INCORRECT RESULT: %d, SHOULD BE: 5", dout);
 
 
-	/* ----------------------
-	   TEST MORE INPUTS 
-	   ---------------------
-	*/
+    /* ----------------------
+     *    TEST MORE INPUTS 
+     * ---------------------
+     */
 
     // Done
-	$stop;
+
+
+
+    $stop;
   end
 endmodule
 
