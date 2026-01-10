30041a23cec4beadeebb1e8eff71bb4da6b686c2
jacobwshg <wsg438@gmail.com>
2026-01-10 17:19:01 -0600

    hw1 implemented clocked outputs

--- hw1-fib/sv/fibonacci.sv
+++ hw1-fib/sv/fibonacci.sv
@@ -14,15 +14,72 @@ module fibonacci(
   }
   state_t;
 
+  typedef logic [15:0] u16_t;
+
   state_t state;
   state_t nx_state;
 
-  typedef logic [15:0] u16_t;
   u16_t itercnt, r0, r1;
   u16_t nx_itercnt, nx_r0, nx_r1;
 
-  localparam FIB0 = 0;
-  localparam FIB1 = 1;
+  logic done_c;
+  u16_t dout_c;
+
+  localparam FIB0 = 'd0;
+  localparam FIB1 = 'd1;
+
+  /* next state logic */
+  always_comb
+  begin
+    nx_state = state;
+    nx_itercnt = itercnt;
+    nx_r0 = r0;
+    nx_r1 = r1;
+    case ( state )
+      S_IDLE:
+      begin
+        nx_state = start? S_RUN: S_IDLE;
+        //nx_itercnt = itercnt;
+        //nx_r0 = r0;
+        //nx_r1 = r1;
+      end
+      S_RUN:
+      begin
+        if ( done )
+        begin
+          nx_state = S_IDLE;
+          //nx_itercnt = itercnt;
+          //nx_r0 = r0;
+          //nx_r1 = r1;
+        end
+        else
+        begin
+          //nx_state = S_RUN;
+          nx_itercnt = itercnt + 1;
+          nx_r0 = r1;
+          nx_r1 = r0 + r1;
+        end
+      end
+      default:
+      begin
+      end
+    endcase
+  end
+
+  /* output logic */
+  always_comb
+  begin
+    done_c = (itercnt >= din);
+    /* Design: keep DOUT to 0 and update to final result once.
+     * 
+     * Use DONE_C for latest done status. Since tb immediately samples DOUT 
+     * when DONE is set, we want DOUT to be set at the exact same clock edge.
+     * If we used DONE to compute DOUT_C, DOUT won't receive the update before
+     * tb samples it.
+     *
+     * */
+    dout_c = (done_c && (din > 0))? r1: FIB0;
+  end
 
   /* state regs */
   always_ff @ (posedge clk, posedge reset)
@@ -45,50 +102,19 @@ module fibonacci(
     end
   end
 
-  /* next state logic */
-  always_comb
+  /* output regs */
+  always_ff @ (posedge clk, posedge reset)
   begin
-    nx_state = S_IDLE;
-    nx_itercnt = 'b1;
-    nx_r0 = FIB0;
-    nx_r1 = FIB1;
-    unique case ( state )
-      S_IDLE:
-      begin
-        nx_state = start? S_RUN: S_IDLE;
-        nx_itercnt = itercnt;
-        nx_r0 = r0;
-        nx_r1 = r1;
-      end
-      S_RUN:
-      begin
-        if ( done )
-        begin
-          nx_state = S_IDLE;
-          nx_itercnt = itercnt;
-          nx_r0 = r0;
-          nx_r1 = r1;
-        end
-        else
-        begin
-          nx_state = S_RUN;
-          nx_itercnt = itercnt + 1;
-          nx_r0 = r1;
-          nx_r1 = r0 + r1;
-        end
-      end
-      default:
-      begin
-      end
-    endcase
-  end
-
-  /* output logic */
-  always_comb
-  begin
-    /* handle 0th fib number (0) */
-    dout = din? r1: r0;
-    done = (itercnt >= din);
+    if (reset)
+    begin
+        dout <= 'b0;
+        done <= 'b0;
+    end
+    else
+    begin
+        dout <= dout_c;
+        done <= done_c;
+    end
   end
 
 endmodule
