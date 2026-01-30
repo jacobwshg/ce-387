72c041d594d3542c34f76125e159a30ac1d3db7b
jacobwshg <wsg438@gmail.com>
2026-01-30 11:15:32 -0600

    hw3 fixed naming and wiring, synthed

--- hw3-modet/sv/motion_detect_top.sv
+++ hw3-modet/sv/motion_detect_top.sv
@@ -8,114 +8,46 @@ module motion_detect_top
 	input  logic		clock,
 	input  logic		reset,
 
-	input  logic		bg_in_we,
-	input  logic [23:0] bg_in_din,
-	input  logic		frame_in_we,
-	input  logic [23:0] frame_in_din,
+	input  logic		bg_gs_we,
+	input  logic [23:0] bg_gs_din,
+	input  logic		frame_gs_we,
+	input  logic [23:0] frame_gs_din,
+	input  logic		frame_hl_we,
+	input  logic [23:0] frame_hl_din,
 	input  logic		hl_out_re,
 
-	output logic		bg_in_full,
-	output logic		frame_in_full,
-	output logic		out_empty,
-	output logic [7:0]  hl_out_dout
+	output logic		bg_gs_full,
+	output logic		frame_gs_full,
+	output logic		frame_hl_full,
+	output logic		hl_out_empty,
+	output logic [23:0] hl_out_dout
 );
 
-logic [23:0] bg_in_dout;
-logic        bg_in_empty;
-logic        bg_in_re;
-fifo #(
-	.FIFO_BUFFER_SIZE(64),
-	.FIFO_DATA_WIDTH(24)
-) bg_in_fifo (
-	.reset(reset),
-	.wr_clk(clock),
-	.wr_en(bg_in_we),
-	.din(bg_in_din),
-	.full(bg_in_full),
-	.rd_clk(clock),
-	.rd_en(bg_in_re),
-	.dout(bg_in_dout),
-	.empty(bg_in_empty)
-);
+logic [23:0] bg_gs_dout;
+logic        bg_gs_empty;
+logic        bg_gs_re;
 
-logic [23:0] frame_in_dout;
-logic        frame_in_empty;
-logic        frame_in_re;
-fifo #(
-	.FIFO_BUFFER_SIZE(64),
-	.FIFO_DATA_WIDTH(24)
-) frame_in_fifo (
-	.reset(reset),
-	.wr_clk(clock),
-	.wr_en(frame_in_we),
-	.din(frame_in_din),
-	.full(frame_in_full),
-	.rd_clk(clock),
-	.rd_en(frame_in_re),
-	.dout(frame_in_dout),
-	.empty(frame_in_empty)
-);
+logic [23:0] frame_gs_dout;
+logic        frame_gs_empty;
+logic        frame_gs_re;
 
-
-logic [7:0]  hl_out_din;
+logic [23:0] hl_out_din;
 logic        hl_out_full;
 logic        hl_out_we;
-fifo #(
-	.FIFO_BUFFER_SIZE(64),
-	.FIFO_DATA_WIDTH(24)
-) hl_out_fifo (
-	.reset(reset),
-	.wr_clk(clock),
-	.wr_en(out_we),
-	.din(hl_out_din),
-	.full(hl_out_full),
-	.rd_clk(clock),
-	.rd_en(hl_out_re),
-	.dout(hl_out_dout),
-	.empty(hl_out_empty)
-);
 
-logic [7:0] bg_gs_din;
-logic bg_gs_we;
-logic bg_gs_full;
-logic [7:0] bg_gs_dout;
-logic bg_gs_re;
-logic bg_gs_empty;
-fifo #(
-	.FIFO_BUFFER_SIZE(64),
-	.FIFO_DATA_WIDTH(8)
-) bg_gs_fifo (
-	.reset(reset),
-	.wr_clk(clock),
-	.wr_en(bg_gs_we),
-	.din(bg_gs_din),
-	.full(bg_gs_full),
-	.rd_clk(clock),
-	.rd_en(bg_gs_re),
-	.dout(bg_gs_dout),
-	.empty(bg_gs_empty)
-);
+logic [7:0] bg_sub_din;
+logic bg_sub_we;
+logic bg_sub_full;
+logic [7:0] bg_sub_dout;
+logic bg_sub_re;
+logic bg_sub_empty;
 
-logic [7:0] frame_gs_din;
-logic frame_gs_we;
-logic frame_gs_full;
-logic [7:0] frame_gs_dout;
-logic frame_gs_re;
-logic frame_gs_empty;
-fifo #(
-	.FIFO_BUFFER_SIZE(64),
-	.FIFO_DATA_WIDTH(8)
-) frame_gs_fifo (
-	.reset(reset),
-	.wr_clk(clock),
-	.wr_en(frame_gs_we),
-	.din(frame_gs_din),
-	.full(frame_gs_full),
-	.rd_clk(clock),
-	.rd_en(frame_gs_re),
-	.dout(frame_gs_dout),
-	.empty(frame_gs_empty)
-);
+logic [7:0] frame_sub_din;
+logic frame_sub_we;
+logic frame_sub_full;
+logic [7:0] frame_sub_dout;
+logic frame_sub_re;
+logic frame_sub_empty;
 
 logic [7:0] sub_hl_din;
 logic sub_hl_we;
@@ -123,20 +55,6 @@ logic sub_hl_full;
 logic [7:0] sub_hl_dout;
 logic sub_hl_re;
 logic sub_hl_empty;
-fifo #(
-	.FIFO_BUFFER_SIZE(64),
-	.FIFO_DATA_WIDTH(8)
-) sub_hl_fifo (
-	.reset(reset),
-	.wr_clk(clock),
-	.wr_en(sub_hl_we),
-	.din(sub_hl_din),
-	.full(sub_hl_full),
-	.rd_clk(clock),
-	.rd_en(sub_hl_re),
-	.dout(sub_hl_dout),
-	.empty(sub_hl_empty)
-);
 
 logic [23:0] frame_hl_din;
 logic frame_hl_we;
@@ -144,19 +62,110 @@ logic frame_hl_full;
 logic [23:0] frame_hl_dout;
 logic frame_hl_re;
 logic frame_hl_empty;
+
+fifo #(
+	.FIFO_BUFFER_SIZE(64),
+	.FIFO_DATA_WIDTH(24)
+) bg_gs_fifo (
+	.reset (reset),
+	.wr_clk(clock),
+	.wr_en (bg_gs_we),
+	.din   (bg_gs_din),
+	.full  (bg_gs_full),
+	.rd_clk(clock),
+	.rd_en (bg_gs_re),
+	.dout  (bg_gs_dout),
+	.empty (bg_gs_empty)
+);
+
+fifo #(
+	.FIFO_BUFFER_SIZE(64),
+	.FIFO_DATA_WIDTH(24)
+) frame_gs_fifo (
+	.reset (reset),
+	.wr_clk(clock),
+	.wr_en (frame_gs_we),
+	.din   (frame_gs_din),
+	.full  (frame_gs_full),
+	.rd_clk(clock),
+	.rd_en (frame_gs_re),
+	.dout  (frame_gs_dout),
+	.empty (frame_gs_empty)
+);
+
+fifo #(
+	.FIFO_BUFFER_SIZE(64),
+	.FIFO_DATA_WIDTH(24)
+) hl_out_fifo (
+	.reset (reset),
+	.wr_clk(clock),
+	.wr_en (hl_out_we),
+	.din   (hl_out_din),
+	.full  (hl_out_full),
+	.rd_clk(clock),
+	.rd_en (hl_out_re),
+	.dout  (hl_out_dout),
+	.empty (hl_out_empty)
+);
+
+fifo #(
+	.FIFO_BUFFER_SIZE(64),
+	.FIFO_DATA_WIDTH(8)
+) bg_sub_fifo (
+	.reset (reset),
+	.wr_clk(clock),
+	.wr_en (bg_sub_we),
+	.din   (bg_sub_din),
+	.full  (bg_sub_full),
+	.rd_clk(clock),
+	.rd_en (bg_sub_re),
+	.dout  (bg_sub_dout),
+	.empty (bg_sub_empty)
+);
+
+fifo #(
+	.FIFO_BUFFER_SIZE(64),
+	.FIFO_DATA_WIDTH(8)
+) frame_sub_fifo (
+	.reset (reset),
+	.wr_clk(clock),
+	.wr_en (frame_sub_we),
+	.din   (frame_sub_din),
+	.full  (frame_sub_full),
+	.rd_clk(clock),
+	.rd_en (frame_sub_re),
+	.dout  (frame_sub_dout),
+	.empty (frame_sub_empty)
+);
+
+fifo #(
+	.FIFO_BUFFER_SIZE(64),
+	.FIFO_DATA_WIDTH(8)
+) sub_hl_fifo (
+	.reset (reset),
+	.wr_clk(clock),
+	.wr_en (sub_hl_we),
+	.din   (sub_hl_din),
+	.full  (sub_hl_full),
+	.rd_clk(clock),
+	.rd_en (sub_hl_re),
+	.dout  (sub_hl_dout),
+	.empty (sub_hl_empty)
+);
+
 fifo #(
 	.FIFO_BUFFER_SIZE(64),
 	.FIFO_DATA_WIDTH(24)
 ) frame_hl_fifo (
-	.reset(reset),
+	.reset (reset),
 	.wr_clk(clock),
-	.wr_en(frame_hl_we),
-	.din(frame_hl_din),
-	.full(frame_hl_full),
+	.wr_en (frame_hl_we),
+	.din   (frame_hl_din),
+	.full  (frame_hl_full),
 	.rd_clk(clock),
-	.rd_en(frame_hl_re),
-	.dout(frame_hl_dout),
-	.empty(frame_hl_empty)
+	.rd_en (frame_hl_re),
+	.dout  (frame_hl_dout),
+	.empty (frame_hl_empty)
 );
 
 grayscale #(
@@ -164,13 +173,13 @@ grayscale #(
 	.clock(clock),
 	.reset(reset),
 
-	.in_empty(bg_in_empty),
-	.in_dout(bg_in_dout),
-	.out_full(bg_gs_full),
+	.in_empty (bg_gs_empty),
+	.in_dout  (bg_gs_dout),
+	.out_full (bg_sub_full),
 
-	.in_rd_en(bg_in_re),
-	.out_wr_en(bg_gs_we)
-	.out_din(bg_gs_din),
+	.in_rd_en (bg_gs_re),
+	.out_wr_en(bg_sub_we),
+	.out_din  (bg_sub_din)
 );
 
 grayscale #(
@@ -178,30 +187,30 @@ grayscale #(
 	.clock(clock),
 	.reset(reset),
 
-	.in_empty(frame_in_empty),
-	.in_dout(frame_in_dout),
-	.out_full(frame_gs_full),
+	.in_empty (frame_gs_empty),
+	.in_dout  (frame_gs_dout),
+	.out_full (frame_sub_full),
 
-	.in_rd_en(frame_in_re),
-	.out_wr_en(frame_gs_we)
-	.out_din(frame_gs_din),
+	.in_rd_en (frame_gs_re),
+	.out_wr_en(frame_sub_we),
+	.out_din  (frame_sub_din)
 );
 
-subtract #( .THRESHOLD( 50 ) ) 
-sub_inst (
+bg_subtract #( .THRESHOLD( 50 ) ) 
+bg_sub_inst (
 	.clock( clock ),
 	.reset( reset ),
 
-	.bg_gs_empty( bg_gs_empty ),
-	.bg_gs_dout( bg_gs_dout ),
-	.frame_gs_empty( frame_gs_empty ),
-	.frame_gs_dout( frame_gs_dout ),
-	.out_full( sub_hl_full ),
+	.bg_sub_empty   ( bg_sub_empty ),
+	.bg_sub_dout    ( bg_sub_dout ),
+	.frame_sub_empty( frame_sub_empty ),
+	.frame_sub_dout ( frame_sub_dout ),
+	.out_full       ( sub_hl_full ),
 
-	.bg_gs_re( bg_gs_re ),
-	.frame_gs_re( frame_gs_re ),
-	.out_we( sub_hl_we ),
-	.out_din( sub_hl_din )
+	.bg_sub_re   ( bg_sub_re ),
+	.frame_sub_re( frame_sub_re ),
+	.out_we      ( sub_hl_we ),
+	.out_din     ( sub_hl_din )
 );
 
 highlight 
@@ -210,16 +219,15 @@ hl_inst(
 	.reset( reset ),
 
 	.gs_sub_empty( sub_hl_empty ),
-	.gs_sub_dout( sub_hl_dout ),
-	.frame_empty( frame_hl_empty ),
-	.frame_dout( frame_hl_dout ),
-	.out_full( hl_out_full ),
+	.gs_sub_dout ( sub_hl_dout ),
+	.frame_empty ( frame_hl_empty ),
+	.frame_dout  ( frame_hl_dout ),
+	.out_full    ( hl_out_full ),
 
 	.gs_sub_re( sub_hl_re ),
-	.frame_re( frame_hl_re ),
-	.out_we( hl_out_we ),
-	.out_din( hl_out_din )
+	.frame_re ( frame_hl_re ),
+	.out_we   ( hl_out_we ),
+	.out_din  ( hl_out_din )
 );
 
 endmodule
-
--- hw3-modet/sv/subtract.sv
+++ hw3-modet/sv/subtract.sv
@@ -1,5 +1,5 @@
 
-module subtract 
+module bg_subtract 
 #(
 	parameter THRESHOLD = 50
 )
@@ -7,14 +7,14 @@ module subtract
 	input  logic		clock,
 	input  logic		reset,
 
-	input  logic		bg_gs_empty,
-	input  logic [7:0]  bg_gs_dout,
-	input  logic		frame_gs_empty,
-	input  logic [7:0]  frame_gs_dout,
+	input  logic		bg_sub_empty,
+	input  logic [7:0]  bg_sub_dout,
+	input  logic		frame_sub_empty,
+	input  logic [7:0]  frame_sub_dout,
 	input  logic		out_full,
 
-	output logic		bg_gs_re,
-	output logic		frame_gs_re,
+	output logic		bg_sub_re,
+	output logic		frame_sub_re,
 	output logic		out_we,
 	output logic [7:0]  out_din
 );
@@ -28,7 +28,7 @@ always_ff @ (posedge clock, posedge reset)
 begin
 	if (reset == 1'b1) begin
 		state <= s0;
-		diff <= 'h0;
+		diff <= 8'h0;
 	end else begin
 		state <= state_c;
 		diff <= diff_c;
@@ -36,8 +36,8 @@ begin
 end
 
 always_comb begin
-	bg_gs_re    = 1'b0;
-	frame_gs_re = 1'b0;
+	bg_sub_re    = 1'b0;
+	frame_sub_re = 1'b0;
 
 	out_we    = 1'b0;
 	out_din   = 'h0;
@@ -46,16 +46,16 @@ always_comb begin
 
 	case (state)
 		s0: begin
-			if ( (!bg_gs_empty) && (!frame_gs_empty) )
+			if ( (!bg_sub_empty) && (!frame_sub_empty) )
 			begin
 				diff_c = 
-					bg_gs_dout > frame_gs_dout
-					? bg_gs_dout - frame_gs_dout
-					: frame_gs_dout - bg_gs_dout; 
-				diff_c = diff_c > THRESHOLD ? 'hff : 'h0;
+					bg_sub_dout > frame_sub_dout
+					? bg_sub_dout - frame_sub_dout
+					: frame_sub_dout - bg_sub_dout; 
+				diff_c = diff_c > THRESHOLD ? 8'hff : 8'h0;
 
-				bg_gs_re    = 'b1;
-				frame_gs_re = 'b1;
+				bg_sub_re    = 'b1;
+				frame_sub_re = 'b1;
 
 				state_c = s1;
 			end
@@ -72,8 +72,8 @@ always_comb begin
 		end
 
 		default: begin
-			bg_gs_re    = 'b0;
-			frame_gs_re = 'b0;
+			bg_sub_re    = 'b0;
+			frame_sub_re = 'b0;
 			out_we      = 'b0;
 			out_din     = 'h0;
 			state_c     = s0;
@@ -84,4 +84,3 @@ always_comb begin
 end
 
 endmodule
-
