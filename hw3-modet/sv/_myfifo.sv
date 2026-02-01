6cb9130f62e6c1634fce1b93919dada9bccfed8f
jacobwshg <wsg438@gmail.com>
2026-02-01 12:38:51 -0600

    hw3 myfifo corrected addr multiple drive

--- hw3-modet/sv/_myfifo.sv
+++ hw3-modet/sv/_myfifo.sv
@@ -18,7 +18,8 @@ module fifo #(
 	function automatic logic [FIFO_DATA_WIDTH-1:0] 
 	to01( input logic [FIFO_DATA_WIDTH-1:0] data );
 		logic [FIFO_DATA_WIDTH-1:0] result;
-		for ( int i=0; i < $bits(data); i++ ) begin
+		for ( int i=0; i < $bits(data); i++ )
+		begin
 			case ( data[i] )  
 				0: result[i] = 1'b0;
 				1: result[i] = 1'b1;
@@ -33,44 +34,31 @@ module fifo #(
 	logic [FIFO_DATA_WIDTH-1:0] fifo_buf [FIFO_BUFFER_SIZE-1:0];
 	logic [FIFO_ADDR_WIDTH-1:0] wr_addr, rd_addr;
 
-	assign empty = 
-		(wr_addr == rd_addr) 
-		? 1'b1 
-		: 1'b0;
+	assign empty = ( wr_addr == rd_addr );
 	assign full = 
 		(wr_addr[FIFO_ADDR_WIDTH-2:0] == rd_addr[FIFO_ADDR_WIDTH-2:0]) 
-		&& (wr_addr[FIFO_ADDR_WIDTH-1] != rd_addr[FIFO_ADDR_WIDTH-1]) 
-		? 1'b1 
-		: 1'b0;
+		&& (wr_addr[FIFO_ADDR_WIDTH-1] != rd_addr[FIFO_ADDR_WIDTH-1]);
 
-	always_ff @ (posedge wr_clk, posedge reset) 
-	begin : reset_wr_addr
-		if ( reset == 1'b1 )
-		begin 
+	always_ff @ ( posedge wr_clk, posedge reset )
+	begin : write_buffer
+		if ( reset )
+		begin
 			wr_addr <= '0;
 		end
-	end
-
-	always_ff @ (posedge wr_clk) 
-	begin : write_buffer
-		if ( (wr_en == 1'b1) && (full == 1'b0) )
+		else if ( (wr_en == 1'b1) && (full == 1'b0) )
 		begin
 			fifo_buf[$unsigned(wr_addr[FIFO_ADDR_WIDTH-2:0])] <= din;
 			wr_addr <= wr_addr + '1;
 		end
 	end
 
-	always_ff @ (posedge rd_clk, posedge reset) 
-	begin : reset_rd_addr
-		if ( reset == 1'b1 )
+	always_ff @ ( posedge rd_clk, posedge reset )
+	begin : read_buffer
+		if ( reset )
 		begin
 			rd_addr <= '0;
 		end
-	end
-
-	always_ff @ (posedge rd_clk) 
-	begin : read_buffer
-		if ( (rd_en == 1'b1) && (empty == 1'b0) )
+		else if ( (rd_en == 1'b1) && (empty == 1'b0) )
 		begin
 			dout <= to01(fifo_buf[$unsigned(rd_addr[FIFO_ADDR_WIDTH-2:0])]);
 			rd_addr <= rd_addr + '1;
