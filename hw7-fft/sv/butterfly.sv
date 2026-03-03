9c4b4d436af5ad17921f40aea9d60a6abe809d33
jacobwshg <wsg438@gmail.com>
2026-03-03 17:56:52 -0600

    hw7 fixed dequant math

--- hw7-fft/sv/butterfly.sv
+++ hw7-fft/sv/butterfly.sv
@@ -21,17 +21,23 @@ module butterfly
  	 */
 	function automatic logic signed [ DATA_WIDTH-1:0 ]
 	DEQUANT( logic signed [ DATA_WIDTH-1:0 ] qq );
-		logic signed [ DATA_WIDTH-1:0 ] q = qq + $signed( Q_STEP/2 ) / $signed( Q_STEP );
+		logic signed [ DATA_WIDTH-1:0 ] q = ( qq + $signed( Q_STEP/2 ) ) / $signed( Q_STEP );
 		return q;
 	endfunction
 
 	logic signed [ 0:1 ] [ DATA_WIDTH-1:0 ] v;
 
+	logic signed [ DATA_WIDTH-1:0 ] rw_r2, iw_i2, rw_i2, iw_r2;
+
 	always_comb
 	begin
 		// w * in2
-		v[RE] = DEQUANT( w[RE] * in2[RE] ) - DEQUANT( w[IM] * in2[IM] );
-		v[IM] = DEQUANT( w[RE] * in2[IM] ) + DEQUANT( w[IM] * in2[RE] );
+		rw_r2 = DEQUANT( w[RE] * in2[RE] );
+		iw_i2 = DEQUANT( w[IM] * in2[IM] );
+		rw_i2 = DEQUANT( w[RE] * in2[IM] );
+		iw_r2 = DEQUANT( w[IM] * in2[RE] );
+		v[RE] = rw_r2 - iw_i2;
+		v[IM] = rw_i2 + iw_r2;
 
 		out1[RE] = in1[RE] + v[RE];
 		out1[IM] = in1[IM] + v[IM];
@@ -39,7 +45,12 @@ module butterfly
 		out2[RE] = in1[RE] - v[RE];
 		out2[IM] = in1[IM] - v[IM];
 
-		//$display( "butterfly: in1: %h+%hj, in2: %h+%hj, w: %h+%hj, out1: %h+%hj, out2: %h+%hj", in1[RE], in1[IM], in2[RE], in2[IM], w[RE], w[IM], out1[RE], out1[IM], out2[RE], out2[IM] );
+		$display( "\nButterfly:" );	
+		$display( "\tw: %08h+%08hj, in1: %08h+%08hj, in2: %08h+%08hj", w[RE], w[IM], in1[RE], in1[IM], in2[RE], in2[IM] );
+		$display( "\tv[RE] = %08h = %08h - %08h ", v[RE], rw_r2, iw_i2 );
+		$display( "\tv[IM] = %08h = %08h - %08h ", v[IM], rw_i2, iw_r2 );
+		$display( "\tout1: %08h+%08hj, out2: %08h+%08hj", out1[RE], out1[IM], out2[RE], out2[IM] );
+		$display( "" );
 
 	end
 
