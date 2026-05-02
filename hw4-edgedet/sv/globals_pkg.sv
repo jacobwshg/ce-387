f22d444bcd5101a59e3e2f0323be0c74c6c3a9d9
jacobwshg <wsg438@gmail.com>
2026-05-02 17:04:29 -0500

    [hw4-edgedet-pipe] added FIFO depth and RGB width to globals

--- hw4-edgedet/sv/globals_pkg.sv
+++ hw4-edgedet/sv/globals_pkg.sv
@@ -6,11 +6,14 @@ package globals_pkg;
 
 	localparam int PX_WIDTH = 8;
 	localparam int SAFE_PX_WIDTH = PX_WIDTH + 4;
+	localparam int RGB_WIDTH = 3 * PX_WIDTH;
 
 	localparam int ROW_IDX_WIDTH = $clog2( IMG_HEIGHT );
 	localparam int COL_IDX_WIDTH = $clog2( IMG_WIDTH  );
 
 	localparam int BOX_DIM = 3;
+
+	localparam int FIFO_DEPTH = 32;
 
 endpackage: globals_pkg
 
