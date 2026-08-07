771c2557c02b1b229c8fb95ab916ce6d958453fa
jacobwshg <wsg438@gmail.com>
2026-08-07 15:17:01 -0500

    [hw7-fft] using improved own FIFO

--- hw7-fft/sv/fifo.sv
+++ hw7-fft/sv/fifo.sv
@@ -30,14 +30,7 @@ module fifo #(
 	logic [ FIFO_DATA_WIDTH-1:0 ] fifo_buf [ FIFO_BUFFER_SIZE-1:0 ];
 
 	logic [ FIFO_ADDR_WIDTH:0 ] wr_addr_r;
-	logic [ FIFO_ADDR_WIDTH:0 ] rd_addr_r;
-
-	logic addr_eq, wrap_eq;
-
-	assign addr_eq = wr_addr_r[ FIFO_ADDR_WIDTH-1:0 ] === rd_addr_r[ FIFO_ADDR_WIDTH-1:0 ];
-	assign wrap_eq = wr_addr_r[ FIFO_ADDR_WIDTH ] === rd_addr_r[ FIFO_ADDR_WIDTH ];
-	assign full  = addr_eq && !wrap_eq;
-	assign empty = ( addr_eq && wrap_eq ); 
+	logic [ FIFO_ADDR_WIDTH:0 ] rd_addr_r, rd_addr_next;
 
 	always_ff @ ( posedge wr_clk )
 	begin
@@ -52,18 +45,26 @@ module fifo #(
 		end
 	end
 
+	assign full = (
+		wr_addr_r[ FIFO_ADDR_WIDTH:0 ] ===
+		{ ~rd_addr_r[ FIFO_ADDR_WIDTH ], rd_addr_r[ FIFO_ADDR_WIDTH-1:0 ] }
+	);
+
 	always_ff @ ( posedge rd_clk )
 	begin
 		if ( reset )
 		begin
 			rd_addr_r <= 1'h0;
+			empty <= 1'b1;
 		end
-		else if ( !empty && rd_en )
+		else
 		begin
-			rd_addr_r <= rd_addr_r + 1'h1;
+			rd_addr_r <= rd_addr_next;
+			empty <= ( rd_addr_next === wr_addr_r );
 		end
 	end
 
+	assign rd_addr_next = rd_addr_r + ( ( rd_en && !empty ) ? 1'h1 : 1'h0 );
 	assign dout = fifo_buf[ rd_addr_r[ FIFO_ADDR_WIDTH-1:0 ] ];
 
 endmodule
