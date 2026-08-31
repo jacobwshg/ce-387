d4a7c2b5117c8ab69160cbb7b1a6356a8be322fb
jacobwshg <wsg438@gmail.com>
2026-08-31 12:10:22 -0500

    [hw7-fft] added new FSM-based stage draft

--- hw7-fft/sv/fft_stage.sv
+++ hw7-fft/sv/fft_stage.sv
@@ -1,17 +1,11 @@
 
-import globals_pkg :: N;
-import globals_pkg :: DWIDTH;
-import globals_pkg :: printtime;
-import twdls_pkg :: TWDLS;
+import globals_pkg::printtime;
+import quant_pkg::DEQUANT;
 
 module fft_stage #(
-	parameter int STAGE = 3,
-	parameter int N = globals_pkg::N,
-	parameter int DWIDTH = globals_pkg::DWIDTH,
-
-	parameter logic signed [ 0:(1<<( STAGE-1 ) )-1 ] [ 0:1 ] [ DWIDTH-1:0 ]
-		STAGE_TWDLS =
-		twdls_pkg::TWDLS [ STAGE-1 ] [ 0:( 1<<( STAGE-1 ) )-1 ]
+	parameter int DWIDTH = 32,
+	parameter int N = 32,
+	parameter int STAGE = 2
 )
 (
 	input  logic clk,
@@ -25,291 +19,340 @@ module fft_stage #(
 	output logic signed [ 0:1 ] [ DWIDTH-1:0 ] dout,
 	output logic out_wr_en
 );
-	import quant_pkg::DEQUANT;
-
 	localparam int
-		RE = 0, IM = 1;
+		REAL = 0, IMAG = 1;
 
 	/*
-	 * 1-based stage index
-	 * Stage 2: step = 4
-	 * Stage 3: step = 8
+	 * Use 0-based stage index
+	 * STEP = offset between two samples in the same position in adjacent
+	 * butterfly groups
+	 * STAGE 0: [ in1[0] in2[0] ] [ in1[0] ... STEP = 2
+	 * STAGE 1: [ in1[0] in1[1] in2[0] in2[1] ] [ in1[0] ... STEP = 4
 	 */ 
-	localparam int STEP = 1 << STAGE;
-	localparam int HALF_STEP = STEP >> 1;
-	localparam int LOG2_N = $clog2( N );
+	localparam int STEP = 2 << STAGE;
+	localparam int HALF_STEP = STEP / 2;
+	// Sample index within a frame
+	localparam int SAMPLE_ID_WIDTH = $clog2( N );
+	// in1/in2 combined delay buffer and twiddle ROM addr width = log( half step )
+	localparam int MEM_ADDR_WIDTH = ( STAGE===0 ) ? 1 : STAGE;
+	// Position of the bit in a sample idx that is 0 for in1 samples and 1 for
+	// in2 samples
+	localparam int IN2_FLAGBIT_POS = STAGE;
 
-	typedef enum logic [ 2:0 ]
+	typedef enum logic [ 5:0 ]
 	{
-		S_FETCH, S_BF_MUL,
-		S_BF_DQ,
-		S_BF_OUT
+		S_INIT,
+		S_FETCH,
+		S_MUL, S_DQ, S_ADD,
+		S_OUT
 	} fsm_state_t;
-	fsm_state_t fsm_state, fsm_state_c;
+	fsm_state_t fsm_state_r, fsm_state_next;
+
+
+	logic [ SAMPLE_ID_WIDTH:0 ] sample_id_r, sample_id_next;
+	/* 
+ 	 * When the incoming sample is a in1, we ignore the butterfly 
+ 	 * and output the previous butterfly's out2 from buffer;
+ 	 * but when we're in step 0, there is no prev butterfly and our
+ 	 * module output is invalid for both in1 or in2 samples )
+ 	 */
+	logic is_in2;
 
 	/*
-	 * Sample idx in frame, which can be partitioned into step idx and 
-	 * sample idx within step
-	 * The latter's MSB can serve as a flag:
-	 *   0 when in former half step ( butterfly output is invalid ),
-	 *   and 1 when in latter half step,
-	 * and less-significant bits can serve as index into delay buffer
-	 * ( if implementing with BRAM ) or twiddle table
-	 *
+	 * Track whether the frame has progressed to the in2 samples of butterfly
+	 * group 0 ( meaning we can begin outputting valid results )
 	 */
-	logic [ LOG2_N:0 ] sampl_idx, sampl_idx_c;
-	/* 
- 	 * Step idx - has an extra high bit to accommodate final stage, 
-	 * which has all samples within a single step but step sampl_idx couldn't possibly 
-	 * be 0-bit wide. Also, it needs to enter a higher "ghost step" to flush the
-	 * pipeline.
-	 */
-	logic [ LOG2_N-STAGE:0 ] step_idx;
-	/* 
- 	 * Sample belongs in former or latter half of step? 
- 	 * ( when the incoming sample is in a former half step, we ignore
- 	 * the butterfly and output the previous butterfly's out2 from buffer;
- 	 * but when we're in step 0, there is no prev butterfly and our
- 	 * module output is invalid regardless of half-step )
- 	 */
-	logic is_latter_hstep;
-
-	logic out_valid;
-
-	/* Read/write addrs for delay buffer; buffering half a step is enough */
-	//logic [ STAGE-2:0 ] buf_rd_addr, buf_wr_addr;
-
-	//
-	// twiddle factor index ( unique to each sample within a half-step )
-	//
-	logic [ STAGE-2:0 ] twdl_idx;
+	logic out_valid, out_valid_r;
 
 	/* Butterfly and buffer signals */
+	// twiddle factor index ( unique to each sample within a half-step )
+	logic [ MEM_ADDR_WIDTH-1:0 ] w_rd_addr;
+	logic w_rd_en;
+	logic signed [ 0:1 ] [ DWIDTH-1:0 ] w_dout;
+
+	logic [ MEM_ADDR_WIDTH-1:0 ] dly_buf_addr;
 	logic signed [ 0:1 ] [ DWIDTH-1:0 ]
-		out1, out2,
+		dly_buf_din, dly_buf_dout;
+	logic dly_buf_wr_en, dly_buf_rd_en;
+
+	logic signed [ 0:1 ] [ DWIDTH-1:0 ]
+		in1, in1_r,
+		in2,
 		w,
-		buf_din, buf_dout;
+		v,   v_r,
+		out1, out1_r,
+		out2, out2_r;
+
+
+	// clear path for multipliers ( no mux on input/output )
 	logic signed [ 0:1 ] [ DWIDTH-1:0 ]
-		in1, in1_c,
-		in2, in2_c,
-		v, v_c;
-
-	logic buf_wr_en;
-
-	/* intermediate results */
+		mul_in2_r, mul_w_r;
 	logic signed [ DWIDTH-1:0 ]
-		prod_wr_i2r, prod_wr_i2r_c,
-		prod_wi_i2i, prod_wi_i2i_c,
-		prod_wr_i2i, prod_wr_i2i_c,
-		prod_wi_i2r, prod_wi_i2r_c;
+		mul_p1_r, mul_p2_r, mul_p3_r, mul_p4_r;
 
-	//bram #(
-	//	.BRAM_ADDR_WIDTH( STAGE-1 ),
-	//	.BRAM_DATA_WIDTH( 2 * DWIDTH )
- 	//) dly_buf (
-	//	.clock  ( clk ),
-	//
-	//	.rd_addr( buf_rd_addr ),
-	//	.wr_addr( buf_wr_addr ),
-	//
-	//	.wr_en  ( buf_wr_en ),
-	//
- 	//	.din    ( buf_din ),
- 	//	.dout   ( buf_dout )
- 	//); 
+	// dequantize intermediate products
+	logic signed [ DWIDTH-1:0 ]
+		dq_p1, dq_p1_r,
+		dq_p2, dq_p2_r,
+		dq_p3, dq_p3_r,
+		dq_p4, dq_p4_r;
 
+	bram #(
+		.DWIDTH( 2 * DWIDTH ),
+		.ADDR_WIDTH( MEM_ADDR_WIDTH )
+ 	) dly_buf (
+		.clk  ( clk ),
+		.wr_addr( dly_buf_addr ),
+		.rd_addr( dly_buf_addr ),
+		.wr_en( dly_buf_wr_en ),
+		.rd_en( dly_buf_rd_en ),
+ 		.din ( dly_buf_din ),
+ 		.dout( dly_buf_dout )
+ 	); 
+
+	/*
 	shiftreg #(
 		.WIDTH( HALF_STEP ),
 		.DWIDTH( 2 * DWIDTH )
 	) dly_buf (
 		.clk  ( clk ),
-		.sh_en  ( buf_wr_en ),
-		.din    ( buf_din ),
-		.dout   ( buf_dout )
-	); 
+		.sh_en  ( dly_buf_wr_en ),
+		.din    ( dly_buf_din ),
+		.dout   ( dly_buf_dout )
+	);
+	*/ 
 
-	assign { step_idx, is_latter_hstep, twdl_idx }
-		= { sampl_idx[ LOG2_N:STAGE ], sampl_idx[ STAGE-1 ], sampl_idx[ STAGE-2:0 ] };
-
-	//
-	// if we're in step 0, only the latter half step's output ( which is
-	// butterfly 0's out1 ) is valid.
-	//
-	assign out_valid = step_idx!==0 || is_latter_hstep;
+	stage_twd_rom
+	#(
+		.STAGE( STAGE ),
+		.ADDR_WIDTH( MEM_ADDR_WIDTH )
+	) w_rom (
+		.clk( clk ),
+		.rd_addr( w_rd_addr ),
+		.rd_en( w_rd_en ),
+		.dout( w_dout )
+	);
 
 	/*
- 	 * In former half step, read back buffered out2 and send it downstream,
+ 	 * When sample is in1, read back buffered out2 and send it downstream,
  	 * buffer in1
- 	 * In latter half step, read back buffered in1, run butterfly, overwrite
+ 	 * When sample is in2, read back buffered in1, run butterfly, overwrite
  	 * in1 with out2 at same half-step addr in buffer
 	 */
 
-	always_ff @ ( posedge clk )
-	begin: rd_twdl
-		//
-		// read a twiddle factor. twdl_idx is part of sampl_idx,
-		// which is updated on the clk edge between S_BF_OUT and S_FETCH.
-		// the read from the new edge happens on the clk edge
-		// between S_FETCH and S_BF_MUL ( assuming no stall ).
-		//
-		w <= STAGE_TWDLS[ twdl_idx ];
-	end: rd_twdl
+
+	always_ff @( posedge clk )
+	begin
+		if ( rst )
+		begin
+			fsm_state_r <= S_INIT;
+			sample_id_r <= 'h0;
+
+			in1_r [ 0:1 ] <= '{ default: 'sh0 };
+			v_r   [ 0:1 ] <= '{ default: 'sh0 };
+			out1_r[ 0:1 ] <= '{ default: 'sh0 };
+			out2_r[ 0:1 ] <= '{ default: 'sh0 };
+
+			out_valid_r <= 1'b0;
+		end
+		else
+		begin
+			fsm_state_r <= fsm_state_next;
+			sample_id_r <= sample_id_next;
+
+			in1_r [ 0:1 ] <= in1 [ 0:1 ];
+			v_r   [ 0:1 ] <= v   [ 0:1 ];
+			out1_r[ 0:1 ] <= out1[ 0:1 ];
+			out2_r[ 0:1 ] <= out2[ 0:1 ];
+
+			out_valid_r <= out_valid;
+		end
+
+		if ( fsm_state_r === S_DQ )
+		begin
+			dq_p1_r <= dq_p1;
+			dq_p2_r <= dq_p2;
+			dq_p3_r <= dq_p3;
+			dq_p4_r <= dq_p4;
+		end
+
+	end
+
+	always_ff @( posedge clk )
+	begin: mul_reg
+		if ( fsm_state_r === S_FETCH )
+		begin
+			mul_in2_r[ 0:1 ] <= in2[ 0:1 ];
+			mul_w_r  [ 0:1 ] <= w  [ 0:1 ];
+		end
+
+		if ( fsm_state_r === S_MUL )
+		begin
+			mul_p1_r <= mul_in2_r[ REAL ] * mul_w_r[ REAL ];
+			mul_p2_r <= mul_in2_r[ IMAG ] * mul_w_r[ IMAG ];
+			mul_p3_r <= mul_in2_r[ REAL ] * mul_w_r[ IMAG ];
+			mul_p4_r <= mul_in2_r[ IMAG ] * mul_w_r[ REAL ];
+		end
+	end: mul_reg
 
 	always_comb
 	begin
-		fsm_state_c = fsm_state;
+		fsm_state_next = fsm_state_r;
+
+		sample_id_next = sample_id_r;
 
 		in_rd_en = 1'b0;
+
+		dly_buf_addr = sample_id_r[ MEM_ADDR_WIDTH-1:0 ];
+		dly_buf_din[ 0:1 ] = '{ default: 'shX };
+		dly_buf_rd_en = 1'b0;
+		dly_buf_wr_en = 1'b0;
+
+		w_rd_addr = 'hX;
+		w_rd_en = 1'b0;
+
+		is_in2 = ( 1'b1===sample_id_r[ IN2_FLAGBIT_POS ] );
+
+		in1[ 0:1 ] = in1_r[ 0:1 ];
+		in2[ 0:1 ] = '{ default: 'shX };
+		v  [ 0:1 ] = v_r  [ 0:1 ];
+
+		dq_p1 = '{ default: 'shX };
+		dq_p2 = '{ default: 'shX };
+		dq_p3 = '{ default: 'shX };
+		dq_p4 = '{ default: 'shX };
+
+		out1[ 0:1 ] = out1_r[ 0:1 ];
+		out2[ 0:1 ] = out2_r[ 0:1 ];
+
 		out_wr_en = 1'b0;
-		dout = '{ default: 'shX };
+		dout[ 0:1 ] = '{ default: 'shX };
 
-		buf_wr_en = 1'b0;
-		buf_din = '{ default: 'shX };
+		case ( fsm_state_r )
+			S_INIT:
+			begin
+				// in2 path
+				w_rd_addr = sample_id_r[ MEM_ADDR_WIDTH-1:0 ];
+				w_rd_en = 1'b1;
 
-		sampl_idx_c = sampl_idx;
+				fsm_state_next = S_FETCH;
+			end
 
-		in1_c = in1;
-		in2_c = in2;
-		v_c   = v;
-
-		prod_wr_i2r_c = prod_wr_i2r;
-		prod_wi_i2i_c = prod_wi_i2i;
-		prod_wr_i2i_c = prod_wr_i2i;
-		prod_wi_i2r_c = prod_wi_i2r;
-
-		out1 = '{ default: 'shX };
-		out2 = '{ default: 'shX };
-
-		case ( fsm_state )
 			S_FETCH:
 			begin
+				/*
+				 * in2 path
+				 * If we stay in this state waiting for !in_empty,
+				 * since w_rd_en is only 1 in S_INIT, w_dout won't change
+				 */
+				w[ 0:1 ] = w_dout[ 0:1 ];
+
 				if ( !in_empty )
 				begin
+					// in1 path
+					in1[ 0:1 ] = din[ 0:1 ];
+					// in2 path
+					in2[ 0:1 ] = din[ 0:1 ];
 					in_rd_en = 1'b1;
-
-					in2_c = din;
-
-					// only run butterfly if in latter half step
-					fsm_state_c = is_latter_hstep
-						? S_BF_MUL
-						: S_BF_OUT;
+					fsm_state_next = S_MUL;
 				end
 			end
 
-			S_BF_MUL:
+			S_MUL:
 			begin
-				prod_wi_i2r_c = w[ IM ] * in2[ RE ];
-				prod_wi_i2i_c = w[ IM ] * in2[ IM ];
-				prod_wr_i2r_c = w[ RE ] * in2[ RE ];
-				prod_wr_i2i_c = w[ RE ] * in2[ IM ];
+				// both paths: assert read addr
+				//dly_buf_addr = sample_id_r[ MEM_ADDR_WIDTH-1:0 ];
+				dly_buf_rd_en = 1'b1;
 
-				in1_c = buf_dout;
-
-				fsm_state_c = S_BF_DQ;
+				fsm_state_next = S_DQ;
 			end
 
-			S_BF_DQ:
+			S_DQ:
 			begin
-				prod_wr_i2r_c = quant_pkg::DEQUANT( prod_wr_i2r );
-				prod_wi_i2i_c = quant_pkg::DEQUANT( prod_wi_i2i );
-				prod_wr_i2i_c = quant_pkg::DEQUANT( prod_wr_i2i );
-				prod_wi_i2r_c = quant_pkg::DEQUANT( prod_wi_i2r );
 
-				fsm_state_c = S_BF_OUT;
+				if ( !is_in2 )
+				begin
+					// in1 path: decode out2
+					out2[ 0:1 ] = dly_buf_dout[ 0:1 ];
+				end
+				else
+				begin
+					// in2 path: decode in1 and compute v
+					in1[ 0:1 ] = dly_buf_dout[ 0:1 ];
+
+					dq_p1 = DEQUANT( mul_p1_r );
+					dq_p2 = DEQUANT( mul_p2_r );
+					dq_p3 = DEQUANT( mul_p3_r );
+					dq_p4 = DEQUANT( mul_p4_r );
+
+					v[ REAL ] = dq_p1 - dq_p2;
+					v[ IMAG ] = dq_p3 + dq_p4;
+				end
+				fsm_state_next = S_ADD;
 			end
 
-			// keep
-			S_BF_OUT:
+			S_ADD:
+			begin
+				if ( is_in2 )
+				begin
+					// in2 path: compute outputs
+					out1[ REAL ] = in1_r[ REAL ] + v_r[ REAL ];
+					out1[ IMAG ] = in1_r[ IMAG ] + v_r[ IMAG ];
+
+					out2[ REAL ] = in1_r[ REAL ] - v_r[ REAL ];
+					out2[ IMAG ] = in1_r[ IMAG ] - v_r[ IMAG ];
+				end
+
+				fsm_state_next = S_OUT;
+			end
+
+			S_OUT:
 			begin
 				if ( !out_full )
 				begin
-					if ( !is_latter_hstep )
+					if ( !is_in2 )
 					begin
-						//
-						// former half step
-						//
+						// in1 path
+						// output prev butterfly's buffered out2, store in1
+						dout[ 0:1 ] = out2_r[ 0:1 ];
 
-						// output prev butterfly's buffered out2
-						dout    = buf_dout;
-						buf_din = in2;
+						dly_buf_din[ 0:1 ] = in1_r[ 0:1 ];
 					end
 					else
 					begin
-						//
-						// latter half step; butterfly is valid
-						//
-						//v_c[ RE ]  = quant_pkg::DEQUANT( prod_wr_i2r ) - quant_pkg::DEQUANT( prod_wi_i2i );
-						//v_c[ IM ]  = quant_pkg::DEQUANT( prod_wr_i2i ) + quant_pkg::DEQUANT( prod_wi_i2r );
-						v_c [ RE ] = prod_wr_i2r - prod_wi_i2i;
-						v_c [ IM ] = prod_wr_i2i + prod_wi_i2r;
+						// in2 path
+						// output newly computed butterfly's out1, store out2
+						dout[ 0:1 ] = out1_r[ 0:1 ];
 
-						out1[ RE ] = in1[ RE ] + v_c[ RE ];
-						out1[ IM ] = in1[ IM ] + v_c[ IM ];
-						out2[ RE ] = in1[ RE ] - v_c[ RE ];
-						out2[ IM ] = in1[ IM ] - v_c[ IM ];
-						// output newly computed butterfly's out1
-						dout    = out1;
-						buf_din = out2;
-						/*
- 						printtime();
-						$display( "stage %0d, sampl_idx %0d, buf_rd_addr %0d", STAGE, sampl_idx, buf_rd_addr  );
-						$display( "\tw = %08h + %08hj", w[ RE ], w[ IM ] );
-						$display( "\tin1 = %08h + %08hj, in2 = %08h + %08hj", in1[ RE ], in1[ IM ], in2[ RE ], in2[ IM ] );
-						$display( "\tout1 = %08h + %08hj, out2 = %08h + %08hj", out1[ RE ], out1[ IM ], out2[ RE ], out2[ IM ] );
-						$display( "" );
-						*/
+						dly_buf_din[ 0:1 ] = out2_r[ 0:1 ];
 					end
+					dly_buf_wr_en = 1'b1;	
 
-					buf_wr_en = 1'b1;
-					out_wr_en = out_valid? 1'b1: 1'b0;
+					out_valid = out_valid_r || sample_id_r[ IN2_FLAGBIT_POS ];
+					out_wr_en = 1'( out_valid );
 
-					if ( out_wr_en )
-					begin
-						//$display( "stage %0d outputting data %08h+%08hj", STAGE, out1[ RE ], out1[ IM ] );
-					end
+					sample_id_next = sample_id_r + 1'h1;
 
-					sampl_idx_c = sampl_idx + 1'h1;
-					fsm_state_c = S_FETCH;
+					/*
+					* Since we don't return to S_INIT, we read-ahead w in this
+					* state
+					*/
+					w_rd_addr = sample_id_next[ IN2_FLAGBIT_POS ];
+					w_rd_en   = 1'b1;
+					fsm_state_next = S_FETCH;
 				end
 
+			end
+
+			default:
+			begin
+				fsm_state_next = S_INIT;
+				sample_id_next = 'h0;
 			end
 
 		endcase
 
 	end
 
-	always_ff @ ( posedge clk, posedge rst )
-	begin
-		if ( rst )
-		begin
-			fsm_state <= S_FETCH;
-
-			sampl_idx <= 'h0;
-
-			in1 <= '{ default: 'sh0 };
-			in2 <= '{ default: 'sh0 };
-			v   <= '{ default: 'sh0 };
-
-			prod_wr_i2r <= 'sh0;
-			prod_wi_i2i <= 'sh0;
-			prod_wr_i2i <= 'sh0;
-			prod_wi_i2r <= 'sh0;
-		end
-		else
-		begin
-			fsm_state <= fsm_state_c;
-
-			sampl_idx <= sampl_idx_c;
-
-			in1 <= in1_c;
-			in2 <= in2_c;
-			v   <= v_c;
-
-			prod_wr_i2r <= prod_wr_i2r_c;
-			prod_wi_i2i <= prod_wi_i2i_c;
-			prod_wr_i2i <= prod_wr_i2i_c;
-			prod_wi_i2r <= prod_wi_i2r_c;
-		end
-	end
-
 endmodule: fft_stage
+
