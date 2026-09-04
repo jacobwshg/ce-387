60879aab75db75f658204504406fe2b622977816
jacobwshg <wsg438@gmail.com>
2026-09-03 23:38:59 -0500

    [hw7-fft] updated FIFO to register both full and empty

--- hw7-fft/sv/fifo.sv
+++ hw7-fft/sv/fifo.sv
@@ -1,71 +1,91 @@
 
 module fifo #(
-	parameter FIFO_DATA_WIDTH = 32,
-	parameter FIFO_BUFFER_SIZE = 256
+	parameter int DWIDTH = 32,
+	parameter int DEPTH = 16
 )
 (
-	input  logic reset,
-	input  logic wr_clk,
+	input  logic clk,
+	input  logic rst,
+
 	input  logic wr_en,
-	input  logic [ FIFO_DATA_WIDTH-1:0 ] din,
+	input  logic [ DWIDTH-1:0 ] din,
 	output logic full,
-	input  logic rd_clk,
+
 	input  logic rd_en,
-	output logic [ FIFO_DATA_WIDTH-1:0 ] dout,
+	output logic [ DWIDTH-1:0 ] dout,
 	output logic empty
 );
 
-	function automatic logic [ FIFO_DATA_WIDTH-1:0 ] to01(
-		input logic [ FIFO_DATA_WIDTH-1:0 ] data
+	function automatic logic [ DWIDTH-1:0 ]
+	to01(
+		input logic [ DWIDTH-1:0 ] data
 	);
-		logic [ FIFO_DATA_WIDTH-1:0 ] result;
-		for ( int i=0; i < FIFO_DATA_WIDTH; ++i )
+		logic [ DWIDTH-1:0 ] result;
+		for ( int i=0; i < DWIDTH; ++i )
 		begin
 			result[ i ] = ( data[ i ] === 1'b1 ) ? 1'b1 : 1'b0;
 		end;
 		return result;
 	endfunction
 
-	localparam FIFO_ADDR_WIDTH = $clog2( FIFO_BUFFER_SIZE );
-	logic [ FIFO_DATA_WIDTH-1:0 ] fifo_buf [ FIFO_BUFFER_SIZE-1:0 ];
 
-	logic [ FIFO_ADDR_WIDTH:0 ] wr_addr_r;
-	logic [ FIFO_ADDR_WIDTH:0 ] rd_addr_r, rd_addr_next;
+	localparam int ADDR_WIDTH = $clog2( DEPTH );
+	logic [ DWIDTH-1:0 ] mem [ DEPTH-1:0 ];
 
-	always_ff @ ( posedge wr_clk )
-	begin
-		if ( reset )
+	logic [ ADDR_WIDTH:0 ] wr_addr_r, wr_addr_next;
+	logic [ ADDR_WIDTH:0 ] rd_addr_r, rd_addr_next;
+	logic full_next, full_r;
+	logic empty_next, empty_r;
+
+	always_ff @ ( posedge clk )
+	begin: wr_meta_proc
+		if ( rst )
 		begin
 			wr_addr_r <= 'h0;
+			full_r <= 1'b0;
 		end
-		else if ( wr_en && !full )
+		else
 		begin
-			fifo_buf[ wr_addr_r[ FIFO_ADDR_WIDTH-1:0 ] ] <= din;
-			wr_addr_r <= wr_addr_r + 1'h1;
+			wr_addr_r <= wr_addr_next;
+			full_r <= full_next;
 		end
-	end
+	end: wr_meta_proc
 
-	assign full = (
-		wr_addr_r[ FIFO_ADDR_WIDTH:0 ] ===
-		{ ~rd_addr_r[ FIFO_ADDR_WIDTH ], rd_addr_r[ FIFO_ADDR_WIDTH-1:0 ] }
+	always_ff @ ( posedge clk )
+	begin: wr_proc
+		if ( !full_r && wr_en )
+		begin
+			mem[ wr_addr_r[ ADDR_WIDTH-1:0 ] ] <= din;
+		end
+	end: wr_proc
+
+	assign wr_addr_next = wr_addr_r + ( ( !full_r && wr_en ) ? 1'h1 : 1'h0 );
+	assign full_next = (
+		wr_addr_next[ ADDR_WIDTH:0 ] ===
+		{ ~rd_addr_r[ ADDR_WIDTH ], rd_addr_r[ ADDR_WIDTH-1:0 ] }
 	);
 
-	always_ff @ ( posedge rd_clk )
+	assign full = full_r;
+
+	always_ff @ ( posedge clk )
 	begin
-		if ( reset )
+		if ( rst )
 		begin
-			rd_addr_r <= 1'h0;
-			empty <= 1'b1;
+			rd_addr_r <= 'h0;
+			empty_r <= 1'b1;
 		end
 		else
 		begin
 			rd_addr_r <= rd_addr_next;
-			empty <= ( rd_addr_next === wr_addr_r );
+			empty_r <= empty_next; 
 		end
 	end
 
-	assign rd_addr_next = rd_addr_r + ( ( rd_en && !empty ) ? 1'h1 : 1'h0 );
-	assign dout = fifo_buf[ rd_addr_r[ FIFO_ADDR_WIDTH-1:0 ] ];
+	assign rd_addr_next = rd_addr_r + ( ( !empty_r && rd_en ) ? 1'h1 : 1'h0 );
+	assign empty_next = ( rd_addr_next === wr_addr_r );
+
+	assign dout = mem[ rd_addr_r[ ADDR_WIDTH-1:0 ] ];
+	assign empty = empty_r;
 
 endmodule
 
