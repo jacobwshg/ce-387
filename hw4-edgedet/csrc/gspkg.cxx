600692182466599c0e0402fde24e4f22dc79ae95
jacobwshg <wsg438@gmail.com>
2026-09-14 00:15:39 -0500

    [hw4-edgedet] modified csrc to generate grayscale table with size 1024

--- hw4-edgedet/csrc/gspkg.cxx
+++ hw4-edgedet/csrc/gspkg.cxx
@@ -18,7 +18,7 @@ int main( const int argc, const char *argv[] )
 
 	std::fprintf(
 		f,
-		"\tlocalparam logic [ 0:255*3 ] [ 7:0 ]\n"
+		"\tlocalparam logic [ 0:1023 ] [ 7:0 ]\n"
 		"\tGRAYSCALE_TBL = \n"
 		"\t'{"
 	);
